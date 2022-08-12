d9d677b58a1b9133b516f211a25d52a58b2e537c
Richard Rembert <info@rembertdesigns.co>
2022-08-12 13:22:36 -0400

    Adding depth to game

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // ADD time?
 
 var snakeTable = document.querySelector(".snakeTable");
@@ -42,3 +43,65 @@ document.addEventListener("keydown", function(e) {
     startSnake();
   }
 });
+
+// start game
+function startSnake() {
+    modul.classList.add("hidden");
+    // clearInterval(checkPageInterval);
+    snake.time = 1;
+    renderSnake();
+    randomFood();
+    // interval, heart of the game
+    setInt = setInterval(function() {
+      move();
+    }, snake.interval);
+  }
+  
+  // end of game
+  function stopp() {
+    clearInterval(setInt);
+    snake.final = snake.score;
+    start.querySelector("span").innerHTML = snake.final + " Points !";
+    setTimeout(function() {
+      start.querySelector("span").innerHTML = "Play Snake";
+    }, 1500);
+    snake.init();
+    modul.classList.remove("hidden");
+  }
+  
+  // move the snake function
+  function move() {
+    // check if move allowed & then eat food
+    hitFood();
+    hitBorder();
+    hitSnake();
+    // actually move the snake
+    updatePositions();
+    renderSnake();
+    document.addEventListener("keydown", turn);
+    snake.canTurn = 1;
+  }
+  
+  function updatePositions() {
+    // remove last snake part (first snake pos)
+    boxes[snake.position[0][0] + snake.position[0][1] * table.rowsCols].classList.remove("snake");
+    snake.position.shift();
+    // add new snake part
+    var head = snake.position[snake.position.length - 1];
+    switch (snake.direction) {
+      case "left":
+        snake.position.push([head[0] - 1, head[1]]);
+        break;
+      case "up":
+        snake.position.push([head[0], head[1] - 1]);
+        break;
+      case "right":
+        snake.position.push([head[0] + 1, head[1]]);
+        break;
+      case "down":
+        snake.position.push([head[0], head[1] + 1]);
+        break;
+      default:
+        console.log("no direction !");
+    }
+  }
